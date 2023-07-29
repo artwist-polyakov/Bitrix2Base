a2efae621c35626808b99b23fea0f343083d17f0
Aleksandr Poliakov <master@artwist.ru>
2023-07-29 20:27:25 +0300

    первичный импорт благополучно обработан

--- clickhouse_handler.py
+++ clickhouse_handler.py
@@ -1,6 +1,7 @@
 import clickhouse_connect
 from tqdm import tqdm
 import time
+from datetime import datetime
 
 def get_client(host, port, user, password, db_name, https = True):
     return clickhouse_connect.get_client(interface = 'https' if https else 'http', host=host, port=port, username=user, password=password, database=db_name)
@@ -46,27 +47,35 @@ def load_data_to_sql(data, table, fields_matching, host, port, user, password, d
     progress_bar = tqdm(total=total, position=0, leave=True)
     k = 0
 
+    columns_and_types = get_columns_and_types(table, host, port, user, password, db_name)
+    columns = [col['name'] for col in columns_and_types]
+    columns_str = ', '.join(columns)
+
     for row in data:
         row = {fields_matching.get(key, key): value for key, value in row.items()}
         ID = row.get('ID')  # assuming 'ID' is the key for the unique identifier
 
         # Check if there's a record with this id in the table and the sum of 'sign' for each 'version' equals 1
-        response = client.command(f"SELECT version, ID, SUM(sign) as sum_sign FROM {table} WHERE ID = {ID} GROUP BY version, ID HAVING sum_sign = 1")
-        versions_and_rows = response.data
-
-        # For each version, create a copy with sign=-1 and insert it into the table
-        for version_and_row in versions_and_rows:
-            version = version_and_row[0]
-            # Get the values from the database
-            db_row = dict(zip(response.column_names, version_and_row))
-            negative_row = db_row.copy()
-            negative_row['version'] = version
-            negative_row['sign'] = -1
-            client.insert(table, [list(negative_row.values())], column_names=list(negative_row.keys()))
+        with client.query_row_block_stream(f"SELECT {columns_str}, SUM(sign) as sum_sign FROM {table} WHERE ID = {ID} GROUP BY {columns_str} HAVING sum_sign = 1") as stream:
+            for block in stream:
+                for db_row in block:
+                    db_row_dict = dict(zip(stream.source.column_names, db_row))
+                    version = db_row_dict.get('version')
+                    # For each version, create a copy with sign=-1 and insert it into the table
+                    negative_row = db_row_dict.copy()
+                    negative_row['version'] = version
+                    negative_row['sign'] = -1
+                    client.insert(table, [list(negative_row.values())], column_names=list(negative_row.keys()))
 
         # Insert the new data with sign=1 and version as the current timestamp
+        # print( int(time.time()) )
         row['version'] = int(time.time())  # current timestamp
+        # row['version'] = datetime.now()
         row['sign'] = 1
+        # print("Значения для вставки в таблицу:")
+        # print([list(row.values())])
+        # print("Названия столбцов:")
+        # print(list(row.keys()))
         client.insert(table, [list(row.values())], column_names=list(row.keys()))
 
         k += 1
